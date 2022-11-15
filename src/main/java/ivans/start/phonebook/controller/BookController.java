5c9957be178586ae6d53276337927d021141c789
ivan <ivan.sharapov.05@mail.ru>
2022-11-16 00:09:30 +0300

    Made finding and searching

--- src/main/java/ivans/start/phonebook/controller/BookController.java
+++ src/main/java/ivans/start/phonebook/controller/BookController.java
@@ -1,3 +1,5 @@
+// https://github.com/DexTver/PhoneBook/tree/master/src/main/java/ivans/start/phonebook
+
 package ivans.start.phonebook.controller;
 
 
@@ -8,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Controller
 @RestController
@@ -25,7 +30,7 @@ public class BookController {
 
     // curl -X POST localhost:8080/contacts/0/update -H "Content-Type: application/json" -d '{"name": "Ivan", "number": "8-800-555-35-35", "email": "example@gmail.com"}'
     @PostMapping("contacts/{index}/update")
-    public ResponseEntity<String> updateAge(
+    public ResponseEntity<String> update(
             @PathVariable("index") Integer index,
             @RequestBody Contact info) {
         if (index < full_list.size()) {
@@ -37,7 +42,7 @@ public class BookController {
 
     // curl -X DELETE localhost:8080/contacts/0
     @DeleteMapping("contacts/{index}")
-    public ResponseEntity<String> deleteUser(
+    public ResponseEntity<String> deleteContact(
             @PathVariable("index") Integer index) {
         if (index < full_list.size()) {
             full_list.remove((int) index);
@@ -47,18 +52,42 @@ public class BookController {
     }
 
     // curl localhost:8080/contacts
+    // curl localhost:8080/contacts?num=666
+    // curl localhost:8080/contacts?sub=I
     @GetMapping("contacts")
-    public ResponseEntity<List<Contact>> getUsers() {
-        return ResponseEntity.ok(full_list);
+    public ResponseEntity<List<Contact>> getContacts(
+            @RequestParam(value = "num", required = false) String num,
+            @RequestParam(value = "sub", required = false) String sub) {
+        if (num != null) {
+            return ResponseEntity.ok(full_list.stream().filter(x -> x.getName().contains(num)).collect(Collectors.toList()));
+        } else if (sub != null) {
+            return ResponseEntity.ok(full_list.stream().filter(x -> x.getName().contains(sub)).collect(Collectors.toList()));
+        } else {
+            return ResponseEntity.ok(full_list);
+        }
     }
 
     // curl localhost:8080/contacts/0
     @GetMapping("contacts/{index}")
-    public ResponseEntity<String> getUser(
+    public ResponseEntity<String> getContact(
             @PathVariable("index") Integer index) {
         if (index < full_list.size()) {
             return ResponseEntity.ok(full_list.get(index).toString());
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Index out of range");
+    }
+
+    // curl localhost:8080/contacts/sorted
+    // curl localhost:8080/contacts/sorted?param=email
+    @GetMapping("contacts/sorted")
+    public ResponseEntity<List<Contact>> getSortedContacts(
+            @RequestParam(value = "param", required = false) String param) {
+        if (Objects.equals(param, "email")) {
+            return ResponseEntity.ok(full_list.stream().filter(x -> x.getEmail() != null).toList().stream().sorted(Comparator.comparing(Contact::getEmail)).collect(Collectors.toList()));
+        } else if (Objects.equals(param, "number")) {
+            return ResponseEntity.ok(full_list.stream().sorted(Comparator.comparing(Contact::getNumber)).collect(Collectors.toList()));
+        } else {
+            return ResponseEntity.ok(full_list.stream().sorted(Comparator.comparing(Contact::getName)).collect(Collectors.toList()));
+        }
     }
 }
